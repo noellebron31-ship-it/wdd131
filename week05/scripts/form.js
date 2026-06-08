adb62cd5c22dfb7df2121258ea6f3b9b09754c43
yourusername <nsuerolebbron@byupathway.edu>
2026-06-08 19:54:21 -0400

    form.html

---
+++ week05/scripts/form.js
@@ -0,0 +1,25 @@
+const products = [
+  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const productSelect = document.getElementById("productName");
+
+    if (productSelect) {
+        products.forEach(product => {
+            const option = document.createElement("option");
+            option.value = product.id; // Backend value uses the product id
+            
+            // Capitalize strings neatly for user display
+            option.textContent = product.name.split(' ')
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(' ');
+
+            productSelect.appendChild(option);
+        });
+    }
+});
